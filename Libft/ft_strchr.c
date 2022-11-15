f59e0b24293af3e7d39cd127f2ee7a83bc775348
Atitla <aztagsuport@gmail.com>
2022-11-15 23:19:04 +0100

    gitignore + strchr

---
+++ Libft/ft_strchr.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+char * ft_strchr( const char * string, int searchedChar ){
+	int i;
+	char *ptr;
+
+	ptr = (char *)string;
+	while (ptr)
+	{
+		if(*ptr == searchedChar)
+			return(ptr);
+		i++;
+		ptr++;
+	}
+    return(NULL);
+}
