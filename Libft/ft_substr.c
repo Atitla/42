d6efed8e23fce4eace450b5f2fa1d9c2815bb3e7
Atitla <aztagsuport@gmail.com>
2022-11-25 16:47:04 +0100

    hu

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -16,9 +16,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	i;
+
+	if ()
+		return (ft_strdup(""));
 	if (start > ft_strlen(s))
 		return("\0");
-	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (len > ft_strlen(s))
+		substr = (char *)malloc(sizeof(char) * ((ft_strlen(s) - start) + 1));
+	else if (len + start > ft_strlen(s))
+		substr = (char *)malloc(sizeof(char) * len);
+	else
+		substr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!substr)
 		return (NULL);
 	i = 0;
