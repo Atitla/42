6ad19080504fb602ae88eca5e7fe393b852fb99c
Esteban Da Cunha <ecunha@student.42lyon.fr>
2023-01-09 17:01:28 +0100

    libft a corriger + debut gnl

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,9 @@
+char *get_next_line(int fd)
+{
+
+	while (/* condition */)
+	{
+		rtrn = read(fd buff,BUFFER_SIZE)
+	}
+
+}
--- piscine/c00/main.c
+++
@@ -1,8 +0,0 @@
-void ft_print_comb();
-//void ft_print_comb2();
-
-int main()
-{
-	ft_print_comb();
-//	ft_print_comb2();
-}
