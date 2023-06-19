bfa76832db66855297b889fd7d4abac0f5bb5dfe
Esteban Da Cunha <aztagsuport@gmail.com>
2023-06-19 16:59:00 +0200

    gnl comm

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -9,7 +9,13 @@ char	*ft_free(char *buffer, char *buf)
 	return (temp);
 }
 
-// delete line find
+/*
+	-len of the first line
+	-if eol == \0 free buffer return NULL
+	-allocate line = len of file - len of firstline + 1
+	-copy buffer into line
+	-free buffer
+*/
 char	*ft_next(char *buffer)
 {
 	int		i;
@@ -17,58 +23,68 @@ char	*ft_next(char *buffer)
 	char	*line;
 
 	i = 0;
-	// find len of first line
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
-	// if eol == \0 return NULL
 	if (!buffer[i])
 	{
 		free(buffer);
 		return (NULL);
 	}
-	// len of file - len of firstline + 1
 	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
 	i++;
 	j = 0;
-	// line == buffer
 	while (buffer[i])
 		line[j++] = buffer[i++];
 	free(buffer);
 	return (line);
 }
-
+/*
+	-allocate res if don't exist
+	-allocate buffer
+	-while read >= 1
+		-read into buffer
+		-if error return NULL + free buffer
+		-put \0 at end of buffer
+		-res =  res + buffer and free res
+		-if \n in buffer break
+	-free buffer
+	-return res
+*/
 char	*read_file(int fd, char *res)
 {
 	char	*buffer;
 	int		byte_read;
 
-	// malloc if res dont exist
 	if (!res)
 		res = ft_calloc(1, 1);
-	// malloc buffer
 	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	byte_read = 1;
 	while (byte_read > 0)
 	{
-		// while not eof read
 		byte_read = read(fd, buffer, BUFFER_SIZE);
 		if (byte_read == -1)
 		{
 			free(buffer);
 			return (NULL);
 		}
-		// 0 to end for leak
 		buffer[byte_read] = 0;
-		// join and free
 		res = ft_free(res, buffer);
-		// quit if \n find
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
 	free(buffer);
 	return (res);
 }
-
+/*
+	check if buffer is empty
+		yes = return NULL
+		no = find len of first line
+			allocate len of first line + 2
+			if line == \0 return NULL
+			copy of buffer into line
+			if buffer[i] == \n line[i] == \n
+			return line
+*/
 char *ft_line(char *buffer){
 
 	char *line;
@@ -88,17 +104,20 @@ char *ft_line(char *buffer){
 		line[i] = buffer[i];
 		i++;
 	}
-	if (buffer[i] || buffer[i] == '\n')
+	if (buffer[i] == '\n')
 		line[i] = '\n';
 	return (line);
 }
-
+/*
+	-buffer = copy buffer until \n or eof
+	-line = alloc buffer + copy buffer until \n or eof
+	-buffer = copy buffer from \n to next /n or eof
+*/
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
 	char		*line;
 
-	// error handling
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	buffer = read_file(fd, buffer);
