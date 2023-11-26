db385fcc1948271491bd09f29e2ccc12dc3b28a6
Esteban Da Cunha <aztagsuport@gmail.com>
2023-11-26 23:30:22 +0100

    ff

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ecunha <ecunha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 12:49:56 by ecunha            #+#    #+#             */
-/*   Updated: 2023/11/25 19:00:12 by ecunha           ###   ########.fr       */
+/*   Updated: 2023/11/26 23:28:35 by ecunha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,14 +42,7 @@ t_llist	*ft_lstnew(int content)
 	newlist -> next = NULL;
 	return (newlist);
 }
-void radix_sort(t_llist **stack_a_ptr, t_llist **stack_b_ptr)
-{
 
-	int		y;
-	while ( >> y != 0)
-		y++;
-
-}
 void	push_swap(t_llist *lst, void (*f)(int))
 {
 	printf("stack_a:\n");
@@ -65,6 +58,9 @@ void	push_swap(t_llist *lst, void (*f)(int))
 	printf("\n");
 }
 
+	//if (head_a == NULL)
+	//	return (0);
+
 int	main(int argc, char **argv)
 {
 	t_llist		*head_a;
@@ -76,8 +72,6 @@ int	main(int argc, char **argv)
 		return (0);
 	head_a = (t_llist *)malloc(sizeof(t_llist));
 	head_a->next = NULL;
-	if (head_a == NULL)
-		return (0);
 	while (i < argc)
 	{
 		ft_lstadd_front(&head_a, ft_lstnew(atoi(argv[argc - i])));
