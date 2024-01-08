1230a868697da682c5c6e25250e45645dca5ecde
Esteban Da Cunha <aztagsuport@gmail.com>
2024-01-08 17:50:06 +0100

    fdghdj

--- projet_graphique/N.h
+++ projet_graphique/N.h
@@ -6,14 +6,22 @@
 /*   By: ecunha <ecunha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 15:27:31 by ecunha            #+#    #+#             */
-/*   Updated: 2024/01/05 17:05:31 by ecunha           ###   ########.fr       */
+/*   Updated: 2024/01/08 16:43:26 by ecunha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef N_H
 # define N_H
 
-# include <mlx.h>
+# include "mlx.h"
 # include <stdio.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
 #endif
--- projet_graphique/main.c
+++ projet_graphique/main.c
@@ -1,11 +1,60 @@
 #include "N.h"
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	draw_square(t_data *data, int h, int l)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < h)
+	{
+		j = 0;
+		while (j < l)
+			my_mlx_pixel_put(data, i, j++, 0x00FF0000);
+		i++;
+	}
+}
+
+void	draw_triangle(t_data *data, int h, int l)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < h)
+	{
+		j = ;
+		while (j < )
+			my_mlx_pixel_put(data, i, j++, 0x00FF0000);
+		i++;
+	}
+}
+
 int	main(void)
 {
-	void *	mlx;
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
 
 	mlx = mlx_init();
 	if (mlx == NULL)
 		return(1);
 
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+
+	draw_triangle(&img, 1000, 1000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
