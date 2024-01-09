3e1c55405fe80ce5ed6df6d297ed07c42228e063
Esteban Da Cunha <aztagsuport@gmail.com>
2024-01-09 18:51:13 +0100

    fcijgdoio;uigaheo;ruigbaeio;ur

--- projet_graphique/N.h
+++ projet_graphique/N.h
@@ -6,7 +6,7 @@
 /*   By: ecunha <ecunha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 15:27:31 by ecunha            #+#    #+#             */
-/*   Updated: 2024/01/08 16:43:26 by ecunha           ###   ########.fr       */
+/*   Updated: 2024/01/09 18:46:03 by ecunha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 
 # include "mlx.h"
 # include <stdio.h>
+# include <stdlib.h>
+
+# define HEIGHT 1080
+# define LENGHT 1920
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
 
 typedef struct	s_data {
 	void	*img;
@@ -22,6 +31,10 @@ typedef struct	s_data {
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
+	int		color;
+	int		pos_x;
+	int		pos_y;
+	t_vars	ptr;
 }				t_data;
 
 #endif
--- projet_graphique/main.c
+++ projet_graphique/main.c
@@ -1,5 +1,41 @@
 #include "N.h"
 
+
+int	close(int keycode, t_data *vars)
+{
+	(void)keycode;
+	mlx_destroy_image(vars->ptr.mlx, vars->img);
+	//mlx_destroy_display(vars->ptr.mlx);
+	mlx_destroy_window(vars->ptr.mlx, vars->ptr.win);
+	exit (0);
+}
+
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+int	get_t(int trgb)
+{
+	return ((trgb >> 24) & 0xFF);
+}
+
+int	get_r(int trgb)
+{
+	return ((trgb >> 16) & 0xFF);
+}
+
+int	get_g(int trgb)
+{
+	return ((trgb >> 8) & 0xFF);
+}
+
+int	get_b(int trgb)
+{
+	return (trgb & 0xFF);
+}
+
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -22,7 +58,7 @@ void	draw_square(t_data *data, int h, int l)
 	}
 }
 
-void	draw_triangle(t_data *data, int h, int l)
+void	draw_(t_data *data, int h, int l, int color)
 {
 	int	i;
 	int	j;
@@ -30,31 +66,60 @@ void	draw_triangle(t_data *data, int h, int l)
 	i = 0;
 	while (i < h)
 	{
-		j = ;
-		while (j < )
-			my_mlx_pixel_put(data, i, j++, 0x00FF0000);
+		j = 0;
+		while (j < l)
+			my_mlx_pixel_put(data, i, j++, color);
 		i++;
 	}
 }
 
+int	key_hook(int keycode, t_data *vars)
+{
+	if (keycode == 65307)
+		close(keycode, vars);
+	return (1);
+}
+
+void	draw_background(t_data *img)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < LENGHT)
+	{
+		j = 0;
+		while (j < HEIGHT)
+			my_mlx_pixel_put(img, i, j++, img->color);
+		i++;
+	}
+}
+
+int	render_next_frame(t_data *img)
+{
+	draw_square(img, 10, 10);
+	mlx_put_image_to_window(img->ptr.mlx, img->ptr.win, img->img, 0, 0);
+
+	return (0);
+}
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
 	t_data	img;
+	img.color = create_trgb(0, 0, 0, 255);
 
 
-	mlx = mlx_init();
-	if (mlx == NULL)
+	img.ptr.mlx = mlx_init();
+	if (img.ptr.mlx == NULL)
 		return(1);
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.ptr.mlx = mlx_init();
+	img.ptr.win = mlx_new_window(img.ptr.mlx, LENGHT, HEIGHT, "Hello world!");
+	img.img = mlx_new_image(img.ptr.mlx, LENGHT, HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-
-	draw_triangle(&img, 1000, 1000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_loop_hook(img.ptr.mlx, render_next_frame, &img);
+	mlx_key_hook(img.ptr.win, key_hook, &img);
+	mlx_loop(img.ptr.mlx);
+	return (0);
 }
